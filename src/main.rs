a1598955793d0bbe13c525dc1a97f1ac684d6eb0
aether-fiducia <aethersaunders@gmail,com>
2020-07-27 14:40:13 -0300

    Starting dev

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use std::env;
+
+use serenity::prelude::*;
+
+//Build a simple struct to impl the EventHandler trait on
+//That will serve the responses to the message event
+
+struct Handler;
+
+impl EventHandler for Event {
+
+}
+
+fn main() {
+
+    println!("Working dev branch");
+
+}
