2aa1dbb6b5e602ac0824d6daf5714559871576ea
aether-fiducia <aethersaunders@gmail,com>
2020-07-28 14:17:08 -0300

    Done for now

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,70 @@
 use std::env;
+use std::net::TcpStream;
+use std::io::prelude::*;
 
-use serenity::prelude::*;
+use serenity::{
+    prelude::*,
+    model::prelude::*,
+};
 
 //Build a simple struct to impl the EventHandler trait on
 //That will serve the responses to the message event
 struct Handler;
 
+//Add impl for all served events
 impl EventHandler for Handler {
 
+    fn message(&self, ctx: Context, mes: Message) {
+        
+        if mes.author.bot != true {
+            if mes.content.contains("!players") {
+                if let Err(err) = mes.channel_id.say(ctx.http,
+                                        format!("{} out of 20 players online.",
+                                        query_players("corvuscorax.org:25565").unwrap())) {
+                    println!("{:?}", err);
+                }
+            }
+        }
+    }
+
+    //Dev info to console
+    fn ready(&self, _: Context, ready: Ready) {
+        println!("{} connected\nID: {}", ready.user.name, ready.user.id);
+    }
 }
 
+//Full of panics! for good reason, all these should halt startup
 fn main() {
+    let token = env::var("TOKEN")
+        .expect("Expected a token!\nFormat - $env:TOKEN=TOKEN_HERE\n");
 
-    println!("Working dev branch");
+    let mut client = Client::new(&token, Handler)
+        .expect("Could not create client!");
 
+    client.start()
+        .expect("Failed to establish a connection to the API");
+}
+
+// Never workin
+fn query_players(addr: &str) -> Result<String, std::io::Error> {
+    let mut stream = match TcpStream::connect(&addr) {
+        Ok(s) => s,
+        Err(err) => panic!("{}", err),
+    };
+    println!("Successful connection");
+
+    let buf = [0xFE, 0x001];
+    stream.write(&buf)?;
+
+    let temp = stream.bytes()
+        .map(|b| match b {
+            Ok(d) => char::from(d),
+            Err(_) => char::from(0u8),
+        })
+        .collect::<String>();
+    let result = temp.split("\x00\x00\x00")
+        .nth(4)
+        .unwrap();
+
+    Ok(result.to_string())
 }
